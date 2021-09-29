34f42915da8d91c66071ff412d67f87bd99c23f0
finowo <whittaker.fin@gmail.com>
2021-09-29 17:07:31 +0100

    why did i even make a scss file lmaooo

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-
-import Vue from 'vue'
-import { BootstrapVue } from 'bootstrap-vue'
-
-import './app.scss'
 
 Vue.use(BootstrapVue)
 
