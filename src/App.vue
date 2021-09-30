c52635baad769e8a3ff4e9a380911b0932a72e1d
finowo <whittaker.fin@gmail.com>
2021-09-30 13:50:58 +0100

    fixed styling of the items

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.card-header {
+  text-align: center;
 }
 </style>
