aca4fac7798eaa64491ff94a73deb03333cce337
finowo <whittaker.fin@gmail.com>
2021-09-30 14:08:29 +0100

    trying to make the done button work and failing

--- src/components/toDoItem.vue
+++ src/components/toDoItem.vue
@@ -1,6 +1,17 @@
 <template>
   <b-list-group-item>
-    {{ toDo.text }}
+    <span v-if="toDo.done" class="done">{{ toDo.text }}</span>
+    <span v-else>{{ toDo.text }}</span>
+    <span v-if="toDo.done" class="float-end">&#128077;</span>
+    <b-button
+      v-else
+      class="float-end"
+      v-model="markAsDone"
+      pill
+      variant="outline-success"
+      @click="markToDoItemAsDone()"
+      >&#10003;</b-button
+    >
   </b-list-group-item>
 </template>
 
@@ -13,8 +24,16 @@ export default {
       required: true,
     },
   },
+  methods: {
+    markToDoItemAsDone() {
+      listIndex = list.findIndex();
+    },
+  },
 };
 </script>
 
 <style>
+.done {
+  text-decoration: line-through;
+}
 </style>
