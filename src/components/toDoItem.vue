245be2b982c840752b01e2647c56fedc22f8af7d
fin whittaker <n00191578@student.iadt.ie>
2021-10-04 13:46:34 +0100

    adding the complete functionality

--- src/components/toDoItem.vue
+++ src/components/toDoItem.vue
@@ -1,39 +1,39 @@
 <template>
-  <b-list-group-item>
-    <span v-if="toDo.done" class="done">{{ toDo.text }}</span>
-    <span v-else>{{ toDo.text }}</span>
-    <span v-if="toDo.done" class="float-end">&#128077;</span>
-    <b-button
-      v-else
-      class="float-end"
-      v-model="markAsDone"
-      pill
-      variant="outline-success"
-      @click="markToDoItemAsDone()"
-      >&#10003;</b-button
-    >
-  </b-list-group-item>
+	<b-list-group-item>
+		<span v-if="toDo.done" class="done">{{ toDo.text }}</span>
+		<span v-else>{{ toDo.text }}</span>
+		<span v-if="toDo.done" class="float-end">&#128077;</span>
+		<b-button
+			v-else
+			class="float-end"
+			v-model="markAsDone"
+			pill
+			variant="outline-success"
+			@click="markToDoItemAsDone()"
+			>&#10003;</b-button
+		>
+	</b-list-group-item>
 </template>
 
 <script>
 export default {
-  name: "toDoItem",
-  props: {
-    toDo: {
-      type: Object,
-      required: true,
-    },
-  },
-  methods: {
-    markToDoItemAsDone() {
-      listIndex = list.findIndex();
-    },
-  },
+	name: "toDoItem",
+	props: {
+		toDo: {
+			type: Object,
+			required: true
+		}
+	},
+	methods: {
+		markToDoItemAsDone() {
+			this.$emit("item-completed", this.toDo);
+		}
+	}
 };
 </script>
 
 <style>
 .done {
-  text-decoration: line-through;
+	text-decoration: line-through;
 }
 </style>
--- src/components/toDoList.vue
+++ src/components/toDoList.vue
@@ -1,58 +1,69 @@
 <template>
-  <div>
-    <b-card header="To-Do List" header-tag="header">
-      <b-list-group>
-        <toDoItem v-for="item in list" :key="item.id" :toDo="item" />
-      </b-list-group>
-      <template v-slot:footer>
-        <input
-          type="text"
-          v-model="toDoText"
-          v-on:keyup.enter="addNewToDoItem()"
-        />
-        <b-button class="float-end" variant="primary" @click="addNewToDoItem()"
-          >Add</b-button
-        >
-      </template>
-    </b-card>
-  </div>
+	<div>
+		<b-card header="To-Do List" header-tag="header">
+			<b-list-group>
+				<toDoItem
+					v-for="item in list"
+					:key="item.id"
+					:toDo="item"
+					@item-completed="markToDoItemAsDone"
+				/>
+			</b-list-group>
+			<template v-slot:footer>
+				<input
+					type="text"
+					v-model="toDoText"
+					v-on:keyup.enter="addNewToDoItem()"
+				/>
+				<b-button
+					class="float-end"
+					variant="primary"
+					@click="addNewToDoItem()"
+					>Add</b-button
+				>
+			</template>
+		</b-card>
+	</div>
 </template>
 
 <script>
 import toDoItem from "@/components/toDoItem.vue";
 
 export default {
-  name: "toDoList",
-  components: {
-    toDoItem,
-  },
-  data() {
-    return {
-      list: [
-        { id: 1, text: "Eat the Cat", done: false },
-        { id: 2, text: "Clean the House", done: true },
-        { id: 3, text: "Hoover the bedroom", done: false },
-      ],
-      toDoText: "",
-    };
-  },
-  methods: {
-    addNewToDoItem() {
-      if (!this.toDoText) {
-        alert("man default js alerts are annoying lmao");
-        return;
-      }
-      const newId =
-        Math.max.apply(
-          null,
-          this.list.map((x) => x.id)
-        ) + 1;
-      this.list.push({ id: newId, text: this.toDoText, done: false });
-      this.toDoText = "";
-    },
-  },
+	name: "toDoList",
+	components: {
+		toDoItem
+	},
+	data() {
+		return {
+			list: [
+				{ id: 1, text: "Eat the Cat", done: false },
+				{ id: 2, text: "Clean the House", done: true },
+				{ id: 3, text: "Hoover the bedroom", done: false }
+			],
+			toDoText: ""
+		};
+	},
+	methods: {
+		addNewToDoItem() {
+			if (!this.toDoText) {
+				alert("man default js alerts are annoying lmao");
+				return;
+			}
+			const newId =
+				Math.max.apply(
+					null,
+					this.list.map(x => x.id)
+				) + 1;
+			this.list.push({ id: newId, text: this.toDoText, done: false });
+			this.toDoText = "";
+		},
+		markToDoItemAsDone(toDo) {
+			const toDoIndex = this.list.indexOf(toDo);
+			this.list[toDoIndex].done = true;
+		}
+	}
 };
 </script>
 
-<style>
-</style>
+<style></style>
