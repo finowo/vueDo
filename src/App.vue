5cf748cacd39dc3b8b861f6015e68ddccf5cdff4
fin whittaker <n00191578@student.iadt.ie>
2021-10-04 13:46:43 +0100

    fixing formatting

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div>
-    <b-container>
-      <b-row>
-        <b-col>
-          <toDoList />
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+	<div>
+		<b-container>
+			<b-row>
+				<b-col>
+					<toDoList />
+				</b-col>
+			</b-row>
+		</b-container>
+	</div>
 </template>
 
 <script>
 import toDoList from "@/components/toDoList.vue";
 export default {
-  name: "App",
-  components: {
-    toDoList,
-  },
+	name: "App",
+	components: {
+		toDoList
+	}
 };
 </script>
 
 <style>
 .container {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 .card-header {
-  text-align: center;
+	text-align: center;
 }
 </style>
